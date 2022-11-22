156652f98ebfb2cedf01f89e50d27fe7459d8084
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2022-11-22 10:24:31 +0100

    Reorganized JS & implemented modal close

--- modal.js
+++ modal.js
@@ -1,3 +1,4 @@
+// Functions
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,17 +8,21 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+
+// Event Listeners
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+modalCloseBtn.addEventListener("click", closeModal);
